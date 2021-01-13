30ce940b5201a28e31c3e36150ad7e94229eabb4
ralibaev <ralibaev@bk.ru>
2021-01-13 19:48:48 +0500

    Добавил php

---
+++ php/data.php
@@ -0,0 +1,14 @@
+$services = [
+  [
+    'name' => 'Исполнительная документация',
+    'link' => '/services/ispolnitelnaya-documentaciya/'
+  ],
+  [
+    'name' => 'Проект производства работ',
+    'link' => '/services/proek-proizvodstva-rabot/'
+  ],
+  [
+    'name' => 'Аутсорсинг ПТО / СКК',
+    'link' => '/services/pto-skk/'
+  ],
+];
